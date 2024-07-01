f54b53ed38d5ec439db90507aa48e759874ec507
Denis <Denis.Engelhardt@SimuTek.de>
2024-07-01 13:05:46 +0200

    gitignore and files

---
+++ README.md
@@ -0,0 +1,2 @@
+Following Vulkan tutorial:
+https://www.youtube.com/watch?v=lr93-_cC8v4&list=PL8327DO66nu9qYVKLDmdLW_84-yE4auCR&index=2
---
+++ lve_window.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <string>
+
+namespace lve {
+
+	class LveWindow {
+	public:
+		LveWindow(int w, int h, std::string name);
+		~LveWindow();
+
+		LveWindow(const LveWindow&) = delete;	//delete copy constructor
+		LveWindow& operator=(const LveWindow &) = delete;	//delete copy operator
+
+		bool shouldClose() { return glfwWindowShouldClose(window); };
+	private:
+		void initWindow();
+
+		const int width;
+		const int height;
+
+		std::string windowName;//asd
+		GLFWwindow* window;
+	};
+}
+
+
